11d5a515eef7aae34e51ca6ed0347bcb70636798
joel23vilca <you@example.com>
2020-04-07 23:51:09 -0500

    react install

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+import React from 'react';
+import ReactDOM from "react-dom";
 const Pet = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Luna"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Havanese"),
-  ]);
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Luna"),
+        React.createElement("h2", {}, "Dog"),
+        React.createElement("h2", {}, "Havanese"),
+    ]);
 };
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
-  ]);
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet),
+        React.createElement(Pet),
+        React.createElement(Pet),
+    ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
