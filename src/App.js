29a8eef3a29f08ba1568fffa5f2feaddf4b52277
joel23vilca <you@example.com>
2020-04-08 10:18:51 -0500

    success react pure in componentes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-const Pet = () => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Luna"),
-        React.createElement("h2", {}, "Dog"),
-        React.createElement("h2", {}, "Havanese"),
-    ]);
-};
+import Pet from './Pet';
 
 const App = () => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet),
-        React.createElement(Pet),
-        React.createElement(Pet),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "Dog",
+            breed: "Havanese"
+        }),
+        React.createElement(Pet, {
+            name: "Pepper",
+            animal: "Bird",
+            breed: "Cockatiel"
+        }),
+        React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
     ]);
 };
 
