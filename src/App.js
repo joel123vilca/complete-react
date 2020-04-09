fac73801dbb4d3a46c9689de4c134e8861d660d1
joel23vilca <you@example.com>
2020-04-09 17:24:45 -0500

    create component searchParams

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
-      <h1> Adopt Me! </h1> <Pet name="Luna" animal="dog" breed="Havanese" />
+      <h1 id="something-important"> Adopt Me! </h1>
+      <SearchParams />
+      <Pet name="Luna" animal="dog" breed="Havanese" />
       <Pet name="Pepper" animal="bird" breed="Cockatiel" />
       <Pet name="Doink" animal="cat" breed="Mix" />
     </div>
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
