849cb67304e5d3deeec630144e7007de3d6aa7f1
joel23vilca <you@example.com>
2020-04-09 19:42:00 -0500

    useEffect use in search

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ const App = () => {
     <div>
       <h1 id="something-important"> Adopt Me! </h1>
       <SearchParams />
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
     </div>
   );
 };
